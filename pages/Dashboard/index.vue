eab89b2d25ef9ccda85a2d31de018379c8ee9629
Mersad Huskic <mersad.huskic@virginpulse.com>
2025-02-21 11:33:59 -0500

    Added inspiration message api

--- pages/Dashboard/index.vue
+++ pages/Dashboard/index.vue
@@ -12,60 +12,84 @@ const apiBaseUrl = config.public.apiBase || 'http://localhost:3080'
 const token = useCookie("token")
 const bearerToken = config.public.bearerToken || token.value
 
-const inpirationMessage = reactive({
-  data: [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis\n" +
-    "            tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit\n" +
-    "            sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora\n" +
-    "            torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac\n" +
-    "            scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel\n" +
-    "            bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.\n" +
-    "            Curabitur tempor quis eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Sed ut vulputate\n" +
-    "            nisi. Integer in felis sed leo vestibulum venenatis. Suspendisse quis arcu sem. Aenean feugiat ex eu\n" +
-    "            vestibulum vestibulum. Morbi a eleifend magna. Nam metus lacus, porttitor eu mauris a, blandit ultrices\n" +
-    "            nibh. Mauris sit amet magna non ligula vestibulum eleifend. Nulla varius volutpat turpis sed lacinia. Nam\n" +
-    "            eget mi in purus lobortis eleifend. Sed nec ante dictum sem condimentum ullamcorper quis venenatis nisi.\n" +
-    "            Proin vitae facilisis nisi, ac posuere leo.",
-    "Nam pulvinar blandit velit, id condimentum diam faucibus at. Aliquam lacus nisi, sollicitudin at nisi nec,\n" +
-    "            fermentum congue felis. Quisque mauris dolor, fringilla sed tincidunt ac, finibus non odio. Sed vitae mauris\n" +
-    "            nec ante pretium finibus. Donec nisl neque, pharetra ac elit eu, faucibus aliquam ligula. Nullam dictum,\n" +
-    "            tellus tincidunt tempor laoreet, nibh elit sollicitudin felis, eget feugiat sapien diam nec nisl. Aenean\n" +
-    "            gravida turpis nisi, consequat dictum risus dapibus a. Duis felis ante, varius in neque eu, tempor suscipit\n" +
-    "            sem. Maecenas ullamcorper gravida sem sit amet cursus. Etiam pulvinar purus vitae justo pharetra consequat.\n" +
-    "            Mauris id mi ut arcu feugiat maximus. Mauris consequat tellus id tempus aliquet.\n" +
-    "            Vestibulum dictum ultrices elit a luctus. Sed in ante ut leo congue posuere at sit amet ligula. Pellentesque\n" +
-    "            eget augue nec nisl sodales blandit sed et sem. Aenean quis finibus arcu, in hendrerit purus. Praesent ac\n" +
-    "            aliquet lorem. Morbi feugiat aliquam ligula, et vestibulum ligula hendrerit vitae. Sed ex lorem, pulvinar\n" +
-    "            sed auctor sit amet, molestie a nibh. Ut euismod nisl arcu, sed placerat nulla volutpat aliquet. Ut id\n" +
-    "            convallis nisl. Ut mauris leo, lacinia sed elit id, sagittis rhoncus odio. Pellentesque sapien libero,\n" +
-    "            lobortis a placerat et, malesuada sit amet dui. Nam sem sapien, congue eu rutrum nec, pellentesque eget\n" +
-    "            ligula.",
-    " Nunc tempor interdum ex, sed cursus nunc egestas aliquet. Pellentesque interdum vulputate elementum. Donec\n" +
-    "            erat diam, pharetra nec enim ut, bibendum pretium tellus. Vestibulum et turpis nibh. Cras vel ornare velit,\n" +
-    "            ac pretium arcu. Cras justo augue, finibus id sollicitudin et, rutrum eget metus. Suspendisse ut mauris eu\n" +
-    "            massa pulvinar sollicitudin vel sed enim. Pellentesque viverra arcu et dignissim vehicula. Donec a velit ac\n" +
-    "            dolor dapibus pellentesque sit amet at erat. Phasellus porttitor, justo eu ultrices vulputate, nisi mi\n" +
-    "            placerat lectus, sed rutrum tellus est id urna. Aliquam pellentesque odio metus, sit amet imperdiet nisl\n" +
-    "            sodales eu. Quisque viverra nunc nec vestibulum dapibus. Integer nec diam a libero tincidunt varius sed vel\n" +
-    "            odio. Donec rutrum dapibus massa, vel tempor nulla porta id. Suspendisse vulputate fermentum sem\n" +
-    "            sollicitudin facilisis. Aliquam vehicula sapien nec ante auctor, quis mollis leo tincidunt.\n" +
-    "            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis\n" +
-    "            tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit\n" +
-    "            sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora\n" +
-    "            torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac\n" +
-    "            scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel\n" +
-    "            bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.\n" +
-    "         "
-  ],
-})
-const dateAndTime = ref("21/02/2025 - 9:34 AM");
+const inspirationMessage = ref('')
+const weatherResponse = ref('')
+const dateAndTime = ref('')
 const errorMessage = ref('')
 const isLoading = ref(false)
-const authStore = useAuthStore();
+const authStore = useAuthStore()
+
 const position = reactive({
   latitude: 0,
   longitude: 0
 })
+
+// Function to get current position
+const getCurrentPosition = () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
+}
+
+// Function to fetch inspiration
+const fetchInspiration = async () => {
+  isLoading.value = true
+  errorMessage.value = ''
+
+  try {
+    // Get current position
+    const geoPosition = await getCurrentPosition()
+    position.latitude = geoPosition.coords.latitude
+    position.longitude = geoPosition.coords.longitude
+
+
+    const config = useRuntimeConfig()
+    console.log("Fetch inspiration")
+    const response = await useFetch(`${apiBaseUrl}/api/v1/ai/inspiration`, {
+      method: 'POST',
+      body: {
+        longitude: position.longitude,
+        latitude: position.latitude
+      },      
+      headers: {
+        Authorization: `Bearer ${bearerToken}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    });
+
+  //  const responseWeather = await useFetch(`${apiBaseUrl}/api/v1/weather`, {
+   //   method: 'GET',
+   //   params: {
+   //     longitude: position.longitude,
+   //     latitude: position.latitude
+   //   },      
+    //  headers: {
+    //    Authorization: `Bearer ${bearerToken}`,
+    //    'Content-Type': 'application/json',
+    //    'Accept': 'application/json',
+    //  },
+   // });
+
+    //weatherResponse = responseWeather.data;
+   // if (!response.ok) {
+    //  throw new Error('Failed to fetch users');
+   // }
+
+ //   if (error.value) throw error.value
+console.log(response)
+    inspirationMessage.value = response.data.value.inspirationalMessage // Adjust according to your API response structure
+    dateAndTime.value = new Date().toLocaleString() // Current date and time
+  } catch (error) {
+    console.error('Error fetching inspiration:', error)
+    errorMessage.value = 'Failed to get inspiration. Please try again.'
+  } finally {
+    isLoading.value = false
+  }
+}
+
+// Call fetchInspiration when the component is mounted
+onMounted(fetchInspiration)
 
 const formSubmit = async () => {
 
@@ -103,7 +127,6 @@ onMounted(() => {
 
 <template>
   <Headers/>
-  <button @click="formSubmit"> clck me</button>
   <div class="p-4">
     <div class="d-flex gap-5">
       <div class="w-50">
@@ -111,7 +134,7 @@ onMounted(() => {
           <div style="width: 60%" class="py-2 w-60 d-flex justify-content-around borderRightLine">
             <div class=" ps-4">
               <div>
-                <div class="curretTemperature">75*F</div>
+                <div class="curretTemperature">75*F </div>
                 <div class="normaltext">Feels like: 65*F</div>
               </div>
               <div class="d-flex flex-column">
@@ -180,9 +203,15 @@ onMounted(() => {
             </div>
           </div>
           <hr/>
-          <div v-for="(data,index)  of inpirationMessage.data" class="label mb-3 ">
-            {{ data }}
-          </div>
+          <div>
+    <p v-if="isLoading">Loading...</p>
+    <p v-else-if="errorMessage">{{ errorMessage }}</p>
+    <div v-else>
+      <p>{{ inspirationMessage }}</p>
+      <p>{{ dateAndTime }}</p>
+    </div>
+    <button @click="fetchInspiration" :disabled="isLoading">Refresh Inspiration</button>
+  </div>
         </div>
       </div>
       <div class="w-50 mt-2">
