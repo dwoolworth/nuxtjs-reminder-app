d0fd8a36ab0ca429eaff1e2a578f9a3fe4f5b6f6
Derrick T. Woolworth <dwoolworth@gmail.com>
2025-02-25 14:57:31 -0500

    fixed up nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2024-11-01',
-    devtools: {enabled: true},
+  compatibilityDate: '2024-11-01',
 
-    css: ['~/assets/css/main.css',
-        'bootstrap/dist/css/bootstrap.min.css',
-        '@fortawesome/fontawesome-svg-core/styles.css'
-    ],
+  devtools: {
+    enabled: true,
 
-    plugins: [
-        {src: '~/plugins/bootstrap.client.ts', mode: 'client'},
-        {src: '~/plugins/fontawesome.ts', mode: 'client'}],
-    modules: ['@vesp/nuxt-fontawesome',
-              '@pinia/nuxt',
-    ],
+    timeline: {
+      enabled: true,
+    },
+  },
 
-    nitro: {
-        devProxy: {
-          '/api': {
-            target: 'http://localhost:3080',
-            changeOrigin: true,
-          }
-        }
-      },
-    runtimeConfig: {
-        public: {
-          apiBase: process.env.API_BASE_URL || 'http://localhost:3080'
-        }
-      },
-    
-      pages: true, 
+  ssr: false,
+
+  css: [
+    '~/assets/css/main.css',
+    'bootstrap/dist/css/bootstrap.min.css',
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ],
+
+  plugins: [
+    { src: '~/plugins/bootstrap.client.ts', mode: 'client' },
+    { src: '~/plugins/fontawesome.ts', mode: 'client' }
+  ],
+
+  modules: [
+    '@vesp/nuxt-fontawesome',
+    '@pinia/nuxt',
+  ],
+
+  nitro: {
+    devProxy: {
+      '/api': {
+        target: 'http://localhost:3080',
+        changeOrigin: true,
+      }
+    }
+  },
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL || 'http://localhost:3080'
+    }
+  },
+
+  pages: true, 
 })
